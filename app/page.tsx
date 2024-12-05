c83cf7b56fcb402c822950169259834c614ed7d7
André Cunha <151823216+andre-mcunha@users.noreply.github.com>
2024-12-05 22:54:02 +0000

    nó no cérbero

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
+'use client';
+
 import React from 'react'
+import useSWR from 'swr';
+import { Product } from "@/app/models/interfaces"
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 
 export default function page() {
+  
+  const { data, error, isLoading } = useSWR<Product[]>('/api/products', fetcher);
+
+  // Validação dos estados de erro ou carregamento
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (!data) return <div>No data available</div>;
+
   return <>
   <h1>Hello World!</h1>
   <p>Bem vindos á minha primeira app desenvolvida em React e Next.js.</p>
---
+++ components/produtctCard/productCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function productCard({title, price, description, image}) {
+  return (
+    <div>productCard</div>
+  )
+}
