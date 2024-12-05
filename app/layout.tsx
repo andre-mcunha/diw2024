467ad8ca39e0102921d9afffc7b06552a37038b7
André Cunha <151823216+andre-mcunha@users.noreply.github.com>
2024-12-05 12:01:58 +0000

    imagem fundo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import './globals.css'
 
 export default function layout({
   children,
