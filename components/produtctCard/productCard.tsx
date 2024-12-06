b671557c70fdf5a63c02a41a186015aac9e72182
André Cunha <151823216+andre-mcunha@users.noreply.github.com>
2024-12-06 22:15:07 +0000

    .....

--- components/produtctCard/productCard.tsx
+++ components/produtctCard/productCard.tsx
@@ -1,6 +1,8 @@
+import { Product } from '@/app/models/interfaces'
 import React from 'react'
 
-export default function productCard({title, price, description, image}) {
+export default function productCard
+({title, price, description, image} : Product) {
   return (
     <div>productCard</div>
   )
