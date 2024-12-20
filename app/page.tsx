b7b4e979bdad13e5e61596c73e73b8f751f28ccd
André Cunha <151823216+andre-mcunha@users.noreply.github.com>
2024-12-20 15:32:57 +0000

    Search implementado (hook)

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import useSWR from 'swr';
 import { Product } from './models/interfaces';
 import ProductCard from './components/ProductCard/ProductCard';
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 
 export default function Page() {
+
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
   const { data, error } = useSWR<Product, Error>('/api/products', fetcher);
+  const [search, setSearch] = useState('');
 
   // Validação dos estados de erro ou carregamento
   if (error) return <div>Failed to load</div>;
@@ -21,19 +24,29 @@ export default function Page() {
     return <div>Invalid data</div>;
   }
 
+ 
+
+  // Filtra os produtos com base no texto digitado
+  const filteredProducts = data.filter((prod) =>
+    prod.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+
   return (
     <div>
     <div className="search-container">
-          <label htmlFor="procurar">Filtros</label>
+          <label htmlFor="search">Filtros</label>
           <input
             id="search"
             type="text"
             placeholder="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)} 
           />
           </div>
 
     <div className='product-grid'>
-      {data.map((prod) => (
+      {filteredProducts.map((prod) => (
         <ProductCard
           key={prod.id}
           title={prod.title}
